7e309a6ddd05089b6944200c565b2231bf57a59f
Paul Easter <paul.john.easter@gmail.com>
2023-11-16 15:16:08 +1100

    Added database project to solution

---
+++ jobsearch_backend_db/dbo/Tables/job_search_terms.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[job_search_terms] (
+    [old_job_id]  INT      NULL,
+    [old_term_id] SMALLINT NULL,
+    [job_id]      INT      NOT NULL,
+    [term_id]     INT      NOT NULL,
+    [valid]       BIT      NOT NULL,
+    CONSTRAINT [PK_job_search_terms] PRIMARY KEY CLUSTERED ([job_id] ASC, [term_id] ASC)
+);
+
---
+++ jobsearch_backend_db/dbo/Tables/search_terms.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[search_terms] (
+    [term_id]     INT            IDENTITY (1, 1) NOT NULL,
+    [old_term_id] INT            NULL,
+    [term_text]   NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_search_terms] PRIMARY KEY CLUSTERED ([term_id] ASC)
+);
+
