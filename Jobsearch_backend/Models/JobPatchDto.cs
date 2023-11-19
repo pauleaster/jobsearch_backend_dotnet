b26598f36381bc0c87e39112d744979003d3ff16
Paul Easter <paul.john.easter@gmail.com>
2023-11-19 15:45:29 +1100

    Partial commit

---
+++ Jobsearch_backend/Models/JobPatchDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Jobsearch_backend.Models
+{
+    public class JobPatchDto
+    {
+        public required int JobId { get; set; }
+        public required int JobNumber { get; set; }
+        public string? JobUrl { get; set; }
+        public string? Title { get; set; }
+        public string? Comments { get; set; }
+        public string? Requirements { get; set; }
+        public string? FollowUp { get; set; }
+        public string? Highlight { get; set; }
+        public string? Applied { get; set; }
+        public string? Contact { get; set; }
+        public string? ApplicationComments { get; set; }
+
+        public override string ToString()
+        {
+            return $"JobId: {JobId}, JobNumber: {JobNumber}, JobUrl: {JobUrl}, Title: {Title}, "
+                    + $"Comments: {Comments}, Requirements: {Requirements}, FollowUp: {FollowUp}, "
+                    + $"Highlight: {Highlight}, Applied: {Applied}, Contact: {Contact}, "
+                    + $"ApplicationComments: {ApplicationComments}";
+        }
+
+    }
+
+    
+}
