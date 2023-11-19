ff66bc42507c78e3c629216a1a863a03e7aa8594
Paul Easter <paul.john.easter@gmail.com>
2023-11-19 16:15:59 +1100

    Partial commit

--- Jobsearch_backend/Models/JobAllowablePatchFields.cs
+++ Jobsearch_backend/Models/JobAllowablePatchFields.cs
@@ -1,4 +1,6 @@
-﻿namespace Jobsearch_backend.Models
+﻿using System.Reflection;
+
+namespace Jobsearch_backend.Models
 {
     public class JobAllowablePatchFields
     {
@@ -11,7 +13,40 @@
         public string Applied = "applied";
         public string Contact = "contact";
         public string ApplicationComments = "application_comments";
+
+        // Check to see if the field name is in the list of allowable fields
+        // using a static method with GetFields and BindingFlags methods
+        public static bool IsAllowableField(string fieldName)
+        {
+            var fields = typeof(JobAllowablePatchFields)
+                .GetFields(BindingFlags.Public |
+                        BindingFlags.Static |
+                        BindingFlags.FlattenHierarchy)
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly);
+            return fields.Any(fi =>
+            {
+                var value = fi.GetValue(null);
+                if (value == null)
+                    continue;
+                else
+                {
+                    var valToString = value.ToString();
+                    if (valToString == null)
+                        return false;
+                    else
+                    {
+                        return valToString.Equals(fieldName, StringComparison.OrdinalIgnoreCase);
+                    }
+                }
+            });
+
+        }
+       
+
+        
     }
 
+
+   
 
 }
