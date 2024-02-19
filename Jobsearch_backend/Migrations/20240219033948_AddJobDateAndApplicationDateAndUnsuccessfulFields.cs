12ea867951f3e3e1431c0d11943b641969ab754e
Paul Easter <paul.john.easter@gmail.com>
2024-02-19 14:53:19 +1100

    Migrations applied for additional job fields.

---
+++ Jobsearch_backend/Migrations/20240219033948_AddJobDateAndApplicationDateAndUnsuccessfulFields.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jobsearch_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobDateAndApplicationDateAndUnsuccessfulFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "application_date",
+                table: "Jobs",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "job_date",
+                table: "Jobs",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "unsuccessful",
+                table: "Jobs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "application_date",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "job_date",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "unsuccessful",
+                table: "Jobs");
+        }
+    }
+}
