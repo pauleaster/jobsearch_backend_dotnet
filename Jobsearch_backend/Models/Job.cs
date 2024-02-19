15f76c73e0a2510b248e6ad96d9814e407a82187
Paul Easter <paul.john.easter@gmail.com>
2024-02-19 12:26:19 +1100

    Partial commit before resetting the migrations.

--- Jobsearch_backend/Models/Job.cs
+++ Jobsearch_backend/Models/Job.cs
@@ -36,12 +36,27 @@
 
         [Column("application_comments")]
         public string? ApplicationComments { get; set; }
+
+        [Column("job_date")]
+        public DateTime? JobDate { get; set; }
+
+        [Column("application_date")]
+        public DateTime? ApplicationDate { get; set; }
+
+        [Column("unsuccessful")]
+        public string? Unsuccessful { get; set; }
+
+
         public override string ToString()
         {
             return $"job_id: {JobId}, job_number: {JobNumber}, job_url: {JobUrl}, title: {Title}, "
-                                   + $"comments: {Comments}, requirements: {Requirements}, follow_up: {FollowUp}, "
-                    + $"highlight: {Highlight}, applied: {Applied}, contact: {Contact}, "
-                    + $"application_comments: {ApplicationComments}";
+                   + $"comments: {Comments}, requirements: {Requirements}, follow_up: {FollowUp}, "
+                   + $"highlight: {Highlight}, applied: {Applied}, contact: {Contact}, "
+                   + $"application_comments: {ApplicationComments}, "
+                   // Adjust the format for JobDate and ApplicationDate
+                   + $"job_date: {JobDate?.ToString("dd/MM/yyyy")}, "
+                   + $"application_date: {ApplicationDate?.ToString("dd/MM/yyyy")}, "
+                   + $"unsuccessful: {Unsuccessful}";
         }
 
     }
